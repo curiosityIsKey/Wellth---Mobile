de892e59c455129d4f79587933c92ab618ffad07
Jason S <jschinis@gmail.com>
2025-06-25 14:27:41 -0400

    Connected Firebase services, updated main to work with additional files to create login screen on web and mobile. Added app.dart and auth_gate.dart files to generate login and registration screens.

---
+++ src/wellth_app/lib/auth_gate.dart
@@ -0,0 +1,81 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key, required this.clientId});
+
+  final String clientId;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(                                       
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          // Creates sign in screen
+          return SignInScreen( 
+            providers: [EmailAuthProvider()],
+            // Create styling and logo display for narrow screens (iOS & Android)
+            headerBuilder: (context, constraints, shrinkOffset) { 
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('assets/flutterfire_300x.png'),
+                ),
+              );
+            },
+            // Adds subtitle depending on if screen is sign in or registration
+            subtitleBuilder: (context, action) {                     
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to FlutterFire, please sign in!')
+                    : const Text('Welcome to Flutterfire, please sign up!'),
+              );
+            },
+            // Adds footer to sign in screen
+            // Default terms and conditions statement
+            footerBuilder: (context, action) {                       
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            // Adds widget to display logo image on widescreens (web and desktop)
+            sideBuilder: (context, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('flutterfire_300x.png'),
+                ),
+              );
+            }, 
+          );
+        }
+
+        return const HomeScreen();
+      },
+    );                                                                
+  }
+}
+
+// ```
+
+// The headerBuilder argument requires a function of the type HeaderBuilder, which
+// is defined in the FlutterFire UI package.
+
+// ```dart
+
+typedef HeaderBuilder = Widget Function(
+ BuildContext context,
+ BoxConstraints constraints,
+ double shrinkOffset,
+);
--- src/wellth_app/lib/main.dart
+++ src/wellth_app/lib/main.dart
@@ -1,52 +1,63 @@
 import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';                  // Added from codelab
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+import 'firebase_options.dart';                                     // Added from codelab
+
+// TODO(codelab user): Get API key
+const clientId = 'YOUR_CLIENT_ID';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp(clientId: clientId));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Namer App',
+//         theme: ThemeData(
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+//         ),
+//         home: MyHomePage(),
+//       ),
+//     );
+//   }
+// }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
+// class MyAppState extends ChangeNotifier {
+//   var current = WordPair.random();
+// }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     var appState = context.watch<MyAppState>();
 
-    return Scaffold(
-      body: Column(
-        children: [Text('A random DOPE idea:'), 
-        Text(appState.current.asLowerCase),
+//     return Scaffold(
+//       body: Column(
+//         children: [Text('A random DOPE idea:'), 
+//         Text(appState.current.asLowerCase),
         
-        ElevatedButton(
-          onPressed: () {
-            print('button pressed!');
-          },
-          child: Text('Next'),
-        ),
+//         ElevatedButton(
+//           onPressed: () {
+//             print('button pressed!');
+//           },
+//           child: Text('Next'),
+//         ),
         
-        ],
-      ),
-    );
-  }
-}
+//         ],
+//       ),
+//     );
+//   }
+// }
